f49fa17582b0fb15d1d4febc3a312052169e9929
masudrahman-dev <masudrahman.dev@gmail>
2023-04-01 09:13:48 +0600

    update question answer

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 const Blog = ({ blog }) => {
   return (
     <div className='mb-12 p-7 dark:text-slate-100 dark:bg-slate-700 rounded-xl shadow-lg'>
-      <h1 className='text-2xl font-bold '>{blog.question}</h1>
-      <p className='text-lg'>{blog.answer}</p>
+      <h1 className='text-2xl  font-bold pb-5 '>{blog.question}</h1>
+      <p className='text-lg leading-8'>{blog.answer}</p>
+      <div className='mockup-code mt-3 '>
+        <pre data-prefix='$'>
+          <code>{blog.answer_example}</code>
+        </pre>
+      </div>
     </div>
   );
 };
